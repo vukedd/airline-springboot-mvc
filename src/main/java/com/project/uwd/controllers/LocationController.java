e0e4423a375f95c17f3de0f55345f18de563926d
vukedd <vukedd@gmail.com>
2024-12-08 20:20:20 +0100

    Location and Register templates refactored using thymeleaf

--- src/main/java/com/project/uwd/controllers/LocationController.java
+++ src/main/java/com/project/uwd/controllers/LocationController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -38,81 +39,81 @@ public class LocationController {
 	}
 	
 	@GetMapping("/")
-	@ResponseBody
-	public String getLocations(@RequestParam(value="status", defaultValue="") String status) {
+	public String getLocations(@RequestParam(value="status", defaultValue="") String status, Model model) {
 		List<Location> locations = _locationService.getLocations();
-		String retval = "<!DOCTYPE html>\r\n"
-				+ "<html lang=\"en\">\r\n"
-				+ "<head>\r\n"
-				+ "    <meta charset=\"UTF-8\">\r\n"
-				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
-				+ "    <title>Document</title>\r\n"
-				+ "</head>\r\n"
-				+ "<body>\r\n"
-				+ "<a href='/user/register'>Register</a><hr>";
-		if (!status.equals("")) {
-			switch (status) {
-				case "error":
-					retval += "<p style='color:red'>Error while deleting the location</p>";
-					break;
-				case "success":
-					retval += "<p style='color:darkGreen'>Location successfully deleted</p>";
-					break;
-				case "added":
-					retval += "<p style='color:darkGreen'>Location successfully added</p>";
-					break;
-			}
-		}
-		
-		for (Location location : locations) {
-			retval += "<a href='/location/details?id=" + location.getId() + "'>" + location.getCity() + "|" + location.getContinent() + "</a><br>";
-		}
-		
-		retval += "<a href='/location/add'>Add new location</a>"
-				+ "</body>\r\n"
-				+ "</html>";
-		
-		return retval;
+		model.addAttribute("locations", locations);
+//		String retval = "<!DOCTYPE html>\r\n"
+//				+ "<html lang=\"en\">\r\n"
+//				+ "<head>\r\n"
+//				+ "    <meta charset=\"UTF-8\">\r\n"
+//				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
+//				+ "    <title>Document</title>\r\n"
+//				+ "</head>\r\n"
+//				+ "<body>\r\n"
+//				+ "<a href='/user/register'>Register</a><hr>";
+//		if (!status.equals("")) {
+//			switch (status) {
+//				case "error":
+//					retval += "<p style='color:red'>Error while deleting the location</p>";
+//					break;
+//				case "success":
+//					retval += "<p style='color:darkGreen'>Location successfully deleted</p>";
+//					break;
+//				case "added":
+//					retval += "<p style='color:darkGreen'>Location successfully added</p>";
+//					break;
+//			}
+//		}
+//		
+//		for (Location location : locations) {
+//			retval += "<a href='/location/details?id=" + location.getId() + "'>" + location.getCity() + "|" + location.getContinent() + "</a><br>";
+//		}
+//		
+//		retval += "<a href='/location/add'>Add new location</a>"
+//				+ "</body>\r\n"
+//				+ "</html>";
+//		
+		return "locations";
 		
 	}
 	
 	@GetMapping("/details")
-	@ResponseBody
-	public String getLocation(@RequestParam("id") Long id, @RequestParam(name="status", defaultValue="") String status) {
+	public String getLocation(@RequestParam("id") Long id, @RequestParam(name="status", defaultValue="") String status, Model model) {
 		Location location = _locationService.getLocation(id);
-		String retval = "<!DOCTYPE html>\r\n"
-				+ "<html lang=\"en\">\r\n"
-				+ "<head>\r\n"
-				+ "    <meta charset=\"UTF-8\">\r\n"
-				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
-				+ "    <title>Document</title>\r\n"
-				+ "</head>\r\n"
-				+ "<body>\r\n";
+		model.addAttribute("location", location);
+//		String retval = "<!DOCTYPE html>\r\n"
+//				+ "<html lang=\"en\">\r\n"
+//				+ "<head>\r\n"
+//				+ "    <meta charset=\"UTF-8\">\r\n"
+//				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
+//				+ "    <title>Document</title>\r\n"
+//				+ "</head>\r\n"
+//				+ "<body>\r\n";
+//		
+//		if (!status.equals("")) {
+//			switch (status) {
+//				case "error":
+//					retval += "<p style='color:red'>Error while editing the location</p>";
+//					break;
+//				case "success":
+//					retval += "<p style='color:darkGreen'>Location successfully edited</p>";
+//					break;
+//			}
+//		}
+//		
+//		if (location != null) {
+//			retval += "<p>" + location.getCity() + "|" + location.getContinent() + "</p>"
+//					+ "<a href='/location/delete?id=" + location.getId() + "'>Delete</a><br>"
+//					+ "<a href='/location/edit?id=" + location.getId() + "'>Edit</a><br>"
+//					+ "<a href='/location/'>Show all locations</a>";
+//		} else {
+//			retval += "<p>Location not found<p>";
+//		}
+//		
+//		retval += "</body>\r\n"
+//				+ "</html>";
 		
-		if (!status.equals("")) {
-			switch (status) {
-				case "error":
-					retval += "<p style='color:red'>Error while editing the location</p>";
-					break;
-				case "success":
-					retval += "<p style='color:darkGreen'>Location successfully edited</p>";
-					break;
-			}
-		}
-		
-		if (location != null) {
-			retval += "<p>" + location.getCity() + "|" + location.getContinent() + "</p>"
-					+ "<a href='/location/delete?id=" + location.getId() + "'>Delete</a><br>"
-					+ "<a href='/location/edit?id=" + location.getId() + "'>Edit</a><br>"
-					+ "<a href='/location/'>Show all locations</a>";
-		} else {
-			retval += "<p>Location not found<p>";
-		}
-		
-		retval += "</body>\r\n"
-				+ "</html>";
-		
-		return retval;
+		return "location-details";
 	}
 	
 	@GetMapping("/delete")
@@ -134,64 +135,73 @@ public class LocationController {
 	}
 	
 	@GetMapping("/add")
-	@ResponseBody
-	public String getAddLocation(@RequestParam(required=false) String city, @RequestParam(required=false) String country, HttpSession session) {
+	public String getAddLocation(@RequestParam(required=false) String city, @RequestParam(required=false) String country, HttpSession session, Model model) {
 		Continent[] continents = Continent.values();
-		String retval = "<!DOCTYPE html>\r\n"
-				+ "<html lang=\"en\">\r\n"
-				+ "<head>\r\n"
-				+ "    <meta charset=\"UTF-8\">\r\n"
-				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
-				+ "    <title>Document</title>\r\n"
-				+ "</head>\r\n"
-				+ "<body>\r\n"
-				+ "    <h1>Add new location</h1>\r\n";
-		Location location = null;
-		boolean invalidTry = false;
-		if (session.getAttribute("location") != null) {
-			location = (Location)session.getAttribute("location");
-			invalidTry = true;
+		model.addAttribute("continents", continents);
+		
+		if (city != null) {
+			model.addAttribute("city", "failure");
 		}
 		
-		retval += "    <form method='Post' action='/location/add'>\r\n"
-// city
-				+ "        <label>City:</label><br>\r\n";
-		if (invalidTry) {
-			retval += "        <input type=\"text\" name=\"city\" value='" + location.getCity() + "'><br>\r\n";
-		} else { 
-			retval += "        <input type=\"text\" name=\"city\"><br>\r\n";
+		if (country != null) {
+			model.addAttribute("country", "failure");
 		}
-		if (city != null && city.equals("failure")) {
-			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>City must contain at least 3 characters</p>";
-		}
-// country
-		retval += "        <label>Country:</label><br>\r\n";
-			
-		if (invalidTry) {
-			retval += "        <input type=\"text\" name=\"country\" value='" + location.getCountry() + "'><br>\r\n";
-		} else { 
-			retval += "        <input type=\"text\" name=\"country\"><br>\r\n";
-		}
-		if (country != null && country.equals("failure")) {
-					retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>Country must contain at least 3 characters</p>";
-				}
-// continent
-		retval += "        <label>Continent:</label><br>\r\n"
-				+ "        <select name=\"continent\">\r\n";
 		
-		for (Continent continent : continents) {
-			if (location != null && location.getContinent().equals(continent)) {
-				retval += "            <option selected value='" + continent + "'>" + continent.name() + "</option>\r\n";
-			}
-			retval += "            <option value='" + continent + "'>" + continent.name() + "</option>\r\n";
-		}
-		retval += "        </select><br>\r\n"
-				+ "   	   <button type='submit'>Add location</button>"
-				+ "   </form>\r\n"
-				+ "</body>\r\n"
-				+ "</html>";
-		
-		return retval;
+//		String retval = "<!DOCTYPE html>\r\n"
+//				+ "<html lang=\"en\">\r\n"
+//				+ "<head>\r\n"
+//				+ "    <meta charset=\"UTF-8\">\r\n"
+//				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
+//				+ "    <title>Document</title>\r\n"
+//				+ "</head>\r\n"
+//				+ "<body>\r\n"
+//				+ "    <h1>Add new location</h1>\r\n";
+//		Location location = null;
+//		boolean invalidTry = false;
+//		if (session.getAttribute("location") != null) {
+//			location = (Location)session.getAttribute("location");
+//			invalidTry = true;
+//		}
+//		
+//		retval += "    <form method='Post' action='/location/add'>\r\n"
+//// city
+//				+ "        <label>City:</label><br>\r\n";
+//		if (invalidTry) {
+//			retval += "        <input type=\"text\" name=\"city\" value='" + location.getCity() + "'><br>\r\n";
+//		} else { 
+//			retval += "        <input type=\"text\" name=\"city\"><br>\r\n";
+//		}
+//		if (city != null && city.equals("failure")) {
+//			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>City must contain at least 3 characters</p>";
+//		}
+//// country
+//		retval += "        <label>Country:</label><br>\r\n";
+//			
+//		if (invalidTry) {
+//			retval += "        <input type=\"text\" name=\"country\" value='" + location.getCountry() + "'><br>\r\n";
+//		} else { 
+//			retval += "        <input type=\"text\" name=\"country\"><br>\r\n";
+//		}
+//		if (country != null && country.equals("failure")) {
+//					retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>Country must contain at least 3 characters</p>";
+//				}
+//// continent
+//		retval += "        <label>Continent:</label><br>\r\n"
+//				+ "        <select name=\"continent\">\r\n";
+//		
+//		for (Continent continent : continents) {
+//			if (location != null && location.getContinent().equals(continent)) {
+//				retval += "            <option selected value='" + continent + "'>" + continent.name() + "</option>\r\n";
+//			}
+//			retval += "            <option value='" + continent + "'>" + continent.name() + "</option>\r\n";
+//		}
+//		retval += "        </select><br>\r\n"
+//				+ "   	   <button type='submit'>Add location</button>"
+//				+ "   </form>\r\n"
+//				+ "</body>\r\n"
+//				+ "</html>";
+//		
+		return "location-add";
 	}
 	
 	@PostMapping("/add")
@@ -234,76 +244,89 @@ public class LocationController {
 	}
 	
 	@GetMapping("/edit")
-	public void getEditLocation(@RequestParam(required=false) String city, @RequestParam(required=false) String country ,@RequestParam(name="status", defaultValue="") String status, @RequestParam(name="id") Long id, HttpServletResponse response, HttpSession session) throws IOException {
+	public String getEditLocation(@RequestParam(required=false) String city, @RequestParam(required=false) String country ,@RequestParam(name="status", defaultValue="") String status, @RequestParam(name="id") Long id, HttpServletResponse response, HttpSession session, Model model) throws IOException {
 		Location location = _locationService.getLocation(id);
-		if (location == null) {
-			response.sendRedirect("/location/");
-			return;
-		}
-		
-		Location locationSession = null;
-		boolean invalidTry = false;
-		if (session.getAttribute("location") != null) {
-			locationSession = (Location)session.getAttribute("location");
-			invalidTry = true;
-		}
-		
-		PrintWriter out = response.getWriter();
+//		if (location == null) {
+//			response.sendRedirect("/location/");
+//			return;
+//		}
+//		
+//		Location locationSession = null;
+//		boolean invalidTry = false;
+//		if (session.getAttribute("location") != null) {
+//			locationSession = (Location)session.getAttribute("location");
+//			invalidTry = true;
+//		}
+//		
+//		PrintWriter out = response.getWriter();
 		Continent[] continents = Continent.values();
-		out.write("<!DOCTYPE html>\r\n"
-				+ "<html lang=\"en\">\r\n"
-				+ "<head>\r\n"
-				+ "    <meta charset=\"UTF-8\">\r\n"
-				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
-				+ "    <title>Document</title>\r\n"
-				+ "</head>\r\n"
-				+ "<body>\r\n"
-				+ "    <h1>Edit location</h1>\r\n");
+//		out.write("<!DOCTYPE html>\r\n"
+//				+ "<html lang=\"en\">\r\n"
+//				+ "<head>\r\n"
+//				+ "    <meta charset=\"UTF-8\">\r\n"
+//				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
+//				+ "    <title>Document</title>\r\n"
+//				+ "</head>\r\n"
+//				+ "<body>\r\n"
+//				+ "    <h1>Edit location</h1>\r\n");
+//		
+//		if (status.equals("error")) {
+//			out.write("<p style='color:red'>Error while editing location</p>");
+//		}
+//		
+//		out.write("    <form method='Post' action='/location/edit?id=" + location.getId() + "'>\r\n"
+//				+ "        <label>City:</label><br>\r\n");
+//		if (invalidTry) {
+//			out.write("        <input type=\"text\" name=\"city\" value='" + locationSession.getCity() + "'><br>\r\n");
+//		} else { 
+//			out.write("        <input type=\"text\" name=\"city\" value='" + location.getCity() + "'><br>\r\n");
+//		}
+//		if (city != null && city.equals("failure")) {
+//			out.write("<p style='color:red;font-size:12px;margin:0;pading:0;'>City must contain at least 3 characters</p>");
+//		}
+//		out.write("        <label>Country:</label><br>\r\n");
+//		if (invalidTry) {
+//			out.write("        <input type=\"text\" name=\"country\" value='" + locationSession.getCountry() + "'><br>\r\n");
+//		} else { 
+//			out.write("        <input type=\"text\" name=\"country\" value='" + location.getCountry() + "'><br>\r\n");
+//		}
+//		if (country != null && country.equals("failure")) {
+//			out.write("<p style='color:red;font-size:12px;margin:0;pading:0;'>Country must contain at least 3 characters</p>");
+//		}
+//		out.write("        <label>Continent:</label><br>\r\n"
+//				+ "        <select name=\"continent\">\r\n");
+//		
+//		for (Continent continent : continents) {
+//			if (locationSession != null && locationSession.getContinent().equals(continent)) {
+//				out.write("            <option selected value='" + continent + "'>" + continent.name() + "</option>\r\n");
+//			} else if (continent.equals(location.getContinent())) {
+//				out.write("            <option selected value='" + continent + "'>" + continent + "</option>\r\n");
+//			}
+//			else {
+//				out.write("            <option value='" + continent + "'>" + continent + "</option>\r\n");
+//			}
+//		}
+//		out.write("        </select><br>\r\n"
+//				+ "   	   <button type='submit'>Save changes</button>"
+//				+ "   </form>\r\n"
+//				+ "<a href='/location/details?id=" + location.getId() + "'>Go back</a>"
+//				+ "</body>\r\n"
+//				+ "</html>");
+//		
+		model.addAttribute("location", location);
+		model.addAttribute("continents", continents);
 		
-		if (status.equals("error")) {
-			out.write("<p style='color:red'>Error while editing location</p>");
+		if (id != null) {
+			model.addAttribute("locationId", id);
+		}
+		if (city != null) {
+			model.addAttribute("city", "failure");
 		}
 		
-		out.write("    <form method='Post' action='/location/edit?id=" + location.getId() + "'>\r\n"
-				+ "        <label>City:</label><br>\r\n");
-		if (invalidTry) {
-			out.write("        <input type=\"text\" name=\"city\" value='" + locationSession.getCity() + "'><br>\r\n");
-		} else { 
-			out.write("        <input type=\"text\" name=\"city\" value='" + location.getCity() + "'><br>\r\n");
+		if (country != null) {
+			model.addAttribute("country", "failure");
 		}
-		if (city != null && city.equals("failure")) {
-			out.write("<p style='color:red;font-size:12px;margin:0;pading:0;'>City must contain at least 3 characters</p>");
-		}
-		out.write("        <label>Country:</label><br>\r\n");
-		if (invalidTry) {
-			out.write("        <input type=\"text\" name=\"country\" value='" + locationSession.getCountry() + "'><br>\r\n");
-		} else { 
-			out.write("        <input type=\"text\" name=\"country\" value='" + location.getCountry() + "'><br>\r\n");
-		}
-		if (country != null && country.equals("failure")) {
-			out.write("<p style='color:red;font-size:12px;margin:0;pading:0;'>Country must contain at least 3 characters</p>");
-		}
-		out.write("        <label>Continent:</label><br>\r\n"
-				+ "        <select name=\"continent\">\r\n");
-		
-		for (Continent continent : continents) {
-			if (locationSession != null && locationSession.getContinent().equals(continent)) {
-				out.write("            <option selected value='" + continent + "'>" + continent.name() + "</option>\r\n");
-			} else if (continent.equals(location.getContinent())) {
-				out.write("            <option selected value='" + continent + "'>" + continent + "</option>\r\n");
-			}
-			else {
-				out.write("            <option value='" + continent + "'>" + continent + "</option>\r\n");
-			}
-		}
-		out.write("        </select><br>\r\n"
-				+ "   	   <button type='submit'>Save changes</button>"
-				+ "   </form>\r\n"
-				+ "<a href='/location/details?id=" + location.getId() + "'>Go back</a>"
-				+ "</body>\r\n"
-				+ "</html>");
-		
-		return;
+		return "location-edit";
 	}
 	
 	@PostMapping("/edit")
--- src/main/java/com/project/uwd/controllers/UserController.java
+++ src/main/java/com/project/uwd/controllers/UserController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -41,92 +42,118 @@ public class UserController {
 	}
 
 	@GetMapping("/register")
-	@ResponseBody
 	public String getRegister(@RequestParam(required = false) String username,
 			@RequestParam(required = false) String email, @RequestParam(required = false) String firstName,
 			@RequestParam(required = false) String lastName, @RequestParam(required = false) String password,
-			@RequestParam(required = false) String dateOfBirth, @RequestParam(required = false) String status, HttpSession session) {
+			@RequestParam(required = false) String dateOfBirth, @RequestParam(required = false) String status, Model model, HttpSession session) {
 		boolean invalidTry = false;
-		User user = null;
 		
-		if (session.getAttribute("registerFormData") != null) {
-			invalidTry = true;
-			user = (User)session.getAttribute("registerFormData");
-		}
-		
-		String retval = "<!DOCTYPE html>\r\n" + "<html lang=\"en\">\r\n" + "<head>\r\n"
-				+ "    <meta charset=\"UTF-8\">\r\n"
-				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
-				+ "    <title>Document</title>\r\n" + "</head>\r\n" + "<body>\r\n"
-				+ "<a href='/location/'>Show all locations</a>"
-				+ "    <h2>Register</h2>\r\n";
-		if (status != null && status.equals("success")) {
-			retval += "<p style='color:green'>User successfully registered!</p>";
-		}
-		// first name
-		retval += "    <form method=\"POST\" action=\"/user/register\">\r\n" +
-				  "        <label>First Name:</label><br>\r\n";
-		
-		if (invalidTry) {
-			retval += "        <input type=\"text\" name=\"firstName\" value='" + user.getFirstName() + "'><br>\r\n";
-		} else {
-			retval += "        <input type=\"text\" name=\"firstName\"><br>\r\n";
-		}
-		if (firstName != null && firstName.equals("failure")) {
-			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>First name must contain only letters</p>";
-		}
-		// last name
-		retval += "        <label>Last Name:</label><br>\r\n";
-		if (invalidTry) {
-			retval += "        <input type=\"text\" name=\"lastName\" value='" + user.getLastName() + "'><br>\r\n";
-		} else {
-			retval += "        <input type=\"text\" name=\"lastName\"><br>\r\n";
-		}	
-		if (lastName != null && lastName.equals("failure")) {
-			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>Last name must contain only letters</p>";
+		model.addAttribute("user", new User());
+		if (email != null) {
+			model.addAttribute("email", "failure");
 		}
 		
-		// DOB
-		retval += "        <label>Date of birth:</label><br>\r\n";
-		if (invalidTry) {
-			retval += 		"        <input type=\"date\" name=\"dateOfBirth\" value='" + user.getDateOfBirth() + "' reqiured><br>\r\n";
-		} else {
-			retval += 		"        <input type=\"date\" name=\"dateOfBirth\" required><br>\r\n";
-		}	
-		if (dateOfBirth != null && dateOfBirth.equals("failure")) {
-			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>Please select date of birth</p>";
+		if (username != null) {
+			model.addAttribute("username", "failure");
 		}
 		
-		// Username
-		retval += "        <label>Username:</label><br>\r\n";
-		if (invalidTry) {
-			retval += "        <input type=\"text\" name=\"username\" value='" + user.getUsername() + "'><br>\r\n";
-		} else {
-			retval += "        <input type=\"text\" name=\"username\"><br>\r\n";
-		}	
-		if (username != null && username.equals("failure")) {
-			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>Username must contain 5 to 20 alphanumerical characters</p>";
+		if (firstName != null) {
+			model.addAttribute("firstName", "failure");
 		}
-		// E-mail
-		retval += "        <label>Email:</label><br>\r\n";
-		if (invalidTry) {
-			retval += "        <input type=\"email\" name=\"email\" value='" + user.getEmail() + "'><br>\r\n";
-		} else {
-			retval += "        <input type=\"email\" name=\"email\"><br>\r\n";
+		
+		if (lastName != null) {
+			model.addAttribute("lastName", "failure");
 		}
-		if (email != null && email.equals("failure")) {
-			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>Please enter a valid email!</p>";
+		
+		if (password != null) {
+			model.addAttribute("password", "failure");
 		}
-		// Password
-		retval += "        <label>Password:</label><br>\r\n"
-				+ "        <input type=\"password\" name=\"password\"><br>\r\n";
-		if (password != null && password.equals("failure")) {
-			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>Password must contain from 8 to 20 characters, at least one letter and at least one digit</p>";
+		
+		if (dateOfBirth != null) {
+			model.addAttribute("dateOfBirth", "failure");
 		}
-		retval += "        <button type=\"submit\">Register</button>\r\n" + "    </form>\r\n" + "</body>\r\n"
-				+ "</html>";
+		
+		if (status != null) {
+			model.addAttribute("status", "success");
+		}
+//		if (session.getAttribute("registerFormData") != null) {
+//			invalidTry = true;
+//			user = (User)session.getAttribute("registerFormData");
+//		}
+//		
+//		String retval = "<!DOCTYPE html>\r\n" + "<html lang=\"en\">\r\n" + "<head>\r\n"
+//				+ "    <meta charset=\"UTF-8\">\r\n"
+//				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
+//				+ "    <title>Document</title>\r\n" + "</head>\r\n" + "<body>\r\n"
+//				+ "<a href='/location/'>Show all locations</a>"
+//				+ "    <h2>Register</h2>\r\n";
+//		if (status != null && status.equals("success")) {
+//			retval += "<p style='color:green'>User successfully registered!</p>";
+//		}
+//		// first name
+//		retval += "    <form method=\"POST\" action=\"/user/register\">\r\n" +
+//				  "        <label>First Name:</label><br>\r\n";
+//		
+//		if (invalidTry) {
+//			retval += "        <input type=\"text\" name=\"firstName\" value='" + user.getFirstName() + "'><br>\r\n";
+//		} else {
+//			retval += "        <input type=\"text\" name=\"firstName\"><br>\r\n";
+//		}
+//		if (firstName != null && firstName.equals("failure")) {
+//			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>First name must contain only letters</p>";
+//		}
+//		// last name
+//		retval += "        <label>Last Name:</label><br>\r\n";
+//		if (invalidTry) {
+//			retval += "        <input type=\"text\" name=\"lastName\" value='" + user.getLastName() + "'><br>\r\n";
+//		} else {
+//			retval += "        <input type=\"text\" name=\"lastName\"><br>\r\n";
+//		}	
+//		if (lastName != null && lastName.equals("failure")) {
+//			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>Last name must contain only letters</p>";
+//		}
+//		
+//		// DOB
+//		retval += "        <label>Date of birth:</label><br>\r\n";
+//		if (invalidTry) {
+//			retval += 		"        <input type=\"date\" name=\"dateOfBirth\" value='" + user.getDateOfBirth() + "' reqiured><br>\r\n";
+//		} else {
+//			retval += 		"        <input type=\"date\" name=\"dateOfBirth\" required><br>\r\n";
+//		}	
+//		if (dateOfBirth != null && dateOfBirth.equals("failure")) {
+//			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>Please select date of birth</p>";
+//		}
+//		
+//		// Username
+//		retval += "        <label>Username:</label><br>\r\n";
+//		if (invalidTry) {
+//			retval += "        <input type=\"text\" name=\"username\" value='" + user.getUsername() + "'><br>\r\n";
+//		} else {
+//			retval += "        <input type=\"text\" name=\"username\"><br>\r\n";
+//		}	
+//		if (username != null && username.equals("failure")) {
+//			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>Username must contain 5 to 20 alphanumerical characters</p>";
+//		}
+//		// E-mail
+//		retval += "        <label>Email:</label><br>\r\n";
+//		if (invalidTry) {
+//			retval += "        <input type=\"email\" name=\"email\" value='" + user.getEmail() + "'><br>\r\n";
+//		} else {
+//			retval += "        <input type=\"email\" name=\"email\"><br>\r\n";
+//		}
+//		if (email != null && email.equals("failure")) {
+//			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>Please enter a valid email!</p>";
+//		}
+//		// Password
+//		retval += "        <label>Password:</label><br>\r\n"
+//				+ "        <input type=\"password\" name=\"password\"><br>\r\n";
+//		if (password != null && password.equals("failure")) {
+//			retval += "<p style='color:red;font-size:12px;margin:0;pading:0;'>Password must contain from 8 to 20 characters, at least one letter and at least one digit</p>";
+//		}
+//		retval += "        <button type=\"submit\">Register</button>\r\n" + "    </form>\r\n" + "</body>\r\n"
+//				+ "</html>";
 
-		return retval;
+		return "register";
 	}
 
 	@PostMapping("/register")
