cff4f302f78fa063d9ac5a4a7c6a01e29b44052e
vukedd <vukedd@gmail.com>
2024-12-14 20:12:49 +0100

    UI changes (Location details, Registration), Log in UI implementation

---
+++ src/main/java/com/project/uwd/controllers/AuthController.java
@@ -0,0 +1,15 @@
+package com.project.uwd.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class AuthController {
+	
+	@GetMapping("/")
+	public String getLogin() {
+		return "log-in";
+	}
+}
